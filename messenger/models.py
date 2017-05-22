6d4b3a68fdf6abf9484d188b52593a6e55879ebd
Rafael Campos <rcampos@mac-rcampos.local>
2017-05-22 11:49:25 -0300

    Removing unwanted default configuration from django's inspectdb

--- messenger/models.py
+++ messenger/models.py
@@ -3,13 +3,6 @@ Created on May 22, 2017
 
 @author: rafaelxy
 """
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from __future__ import unicode_literals
 
 from django.db import models
@@ -34,7 +27,7 @@ class User(models.Model):
         db_table = 'user'
 
 class Conversation(models.Model):
-    creator = models.ForeignKey('User', models.DO_NOTHING, blank=True, null=True)
+    creator = models.ForeignKey('User', blank=True, null=True)
     create_at = models.DateTimeField(blank=True, null=True)
     message_count = models.IntegerField(blank=True, null=True)
 
@@ -45,9 +38,9 @@ class Conversation(models.Model):
 class Message(models.Model):
     text = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(blank=True, null=True)
-    sender = models.ForeignKey('User', models.DO_NOTHING, db_column='sender', blank=True, null=True)
-    receiver = models.ForeignKey('User', models.DO_NOTHING, db_column='receiver', blank=True, null=True)
-    conversation = models.ForeignKey(Conversation, models.DO_NOTHING, blank=True, null=True)
+    sender = models.ForeignKey('User', db_column='sender', blank=True, null=True)
+    receiver = models.ForeignKey('User', db_column='receiver', blank=True, null=True)
+    conversation = models.ForeignKey(Conversation, related_name='messages', blank=True, null=True)
 
     class Meta:
         managed = False
