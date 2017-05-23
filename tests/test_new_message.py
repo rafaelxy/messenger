21ebe16f5507335a754dbfcae31208bef869463b
Rafael Campos <rcampos@mac-rcampos.local>
2017-05-23 09:27:39 -0300

    Cleanup

--- tests/test_new_message.py
+++ tests/test_new_message.py
@@ -69,11 +69,5 @@ class NewMessageTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(Message.objects.count(), 1)
 
-#     def test_new_conversation(self):
-#         pass
-#     def test_user_not_exists(self):
-#         pass
-#     def test_conversation_does_not_exist(self):
-#         pass
 
 
